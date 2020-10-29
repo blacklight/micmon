4e3a02b2cc3d1a832ccde6a2cd9dbf259eb12dfd
Fabio Manganiello <blacklight86@gmail.com>
2020-10-29 14:27:19 +0100

    Logging format

--- micmon/__init__.py
+++ micmon/__init__.py
@@ -1,4 +1,8 @@
 import logging
 import sys
 
-logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+logging.basicConfig(level=logging.INFO,
+                    stream=sys.stdout,
+                    format='[%(asctime)s] %(name)s|%(levelname)-8s|%(message)s',
+                    datefmt='%Y-%m-%d %H:%M:%S')
+
